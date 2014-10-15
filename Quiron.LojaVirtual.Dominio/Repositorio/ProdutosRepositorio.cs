227113599f8734bb90466ad78967511f43af8006
devmediamvc <cintiamayrink@yahoo.com.br>
2014-10-15 18:40:04 -0300

    Aula6 Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+        
+    }
+}
